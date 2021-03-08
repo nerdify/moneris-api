4ac5a02942279e1233ae337564b4b55feb781ef4
sald19 <carl.sald19@gmail.com>
2021-03-08 11:30:35 -0600

    change order field in vault

--- src/Vault.php
+++ src/Vault.php
@@ -199,7 +199,7 @@ class Vault extends Gateway
      *
      * @return \CraigPaul\Moneris\Response
      */
-    public function update($key = '', CreditCard $card, $extra_params = [])
+    public function update(CreditCard $card, $key = '', $extra_params = [])
     {
         $params = array_merge($extra_params, [
             'type' => 'res_update_cc',
